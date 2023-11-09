16151d9d660998d47d479768db432dfda22c6d8e
nsʇnɾ ʇǝɹɔǝs <92639080+SECRET-GUEST@users.noreply.github.com>
2023-11-09 19:58:37 +0100

    Delete src directory

--- src/gui/gui.h
+++
@@ -1,8 +0,0 @@
-#ifndef GUI_H
-#define GUI_H
-
-void run_gui(int argc, char *argv[]);
-
-#endif //GUI_H
-void run_gui(int argc, char *argv[]);
-
--- src/main.c
+++
@@ -1,7 +0,0 @@
-#include "../src/program/program.h"
-#include "../src/gui/gui.h"
-
-int main(int argc, char *argv[]) {
-    run_gui(argc, argv);
-    return 0;
-}
